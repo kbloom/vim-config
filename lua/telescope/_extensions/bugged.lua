18819792e5d35dfce31b8d0ddd67dab35933eba3
Ken Bloom <kabloom@google.com>
2024-07-09 14:13:09 -0400

    Enable multi-selection of bugs with the tab key.
    If any bugs are selected with tab, those are the ones that will be
    inserted. Otherwise insert the one that was highlighted when I hit the
    enter key.

--- lua/telescope/_extensions/bugged.lua
+++ lua/telescope/_extensions/bugged.lua
@@ -4,7 +4,7 @@ local sorters = require "telescope.sorters"
 local conf = require("telescope.config").values
 local actions = require "telescope.actions"
 local action_state = require "telescope.actions.state"
-
+local action_utils = require "telescope.actions.utils"
 
 local insert = function(opts)
   opts = opts or {}
@@ -14,13 +14,28 @@ local insert = function(opts)
     sorter = sorters.get_fuzzy_file(),
     attach_mappings = function(prompt_bufnr, map)
       actions.select_default:replace(function()
+        -- first try to gather the bugs that were selected with the tab key
+        local results = ""
+        action_utils.map_selections(prompt_bufnr, function(entry, index)
+          local bugnum = string.match(entry[1], "%d+")
+          if results ~= "" then
+            results = results .. ","
+          end
+          results = results .. bugnum
+        end)
         actions.close(prompt_bufnr)
+        if results ~= "" then
+          vim.api.nvim_put({ results }, "", false, true)
+          return true
+        end
+        -- if no results were selected with the tab key,
+        -- use the one that was highlighted when I hit enter.
         local selection = action_state.get_selected_entry()
         local bugnum = string.match(selection[1], "%d+")
         vim.api.nvim_put({ bugnum }, "", false, true)
       end)
       return true
-    end,    
+    end,
   }):find()
 end
 
@@ -32,3 +47,4 @@ return require("telescope").register_extension {
     bugged = insert
   },
 }
+
