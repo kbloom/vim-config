16ee53d0537913600ecea070310ab3c94820ce1c
Ken Bloom <kabloom@google.com>
2026-05-20 12:03:56 -0400

    Fix problems with my primery_clipboard plugin.
    1. In recent versions of Neovim, executing a keybiding in normal mode
       was interfering with holding down the arrow key button to select
       multiple lines in visual mode.
    2. It didn't reliably copy the selection to the * buffer anyway.
    3. I think it copied into the * buffer when you used the keyboard to
       select.
    
    This new approach uses a <LeftRelease> key binding to make sure this
    only happens when selecting using the mouse. Both using a <LeftRelease>
    key binding and not emulating keystrokes to do the selection will solve
    the key repeat problem.
    
    Vibe-coded by Gemini after trying too many other solutions.

--- plugin/primary_clipboard.lua
+++ plugin/primary_clipboard.lua
@@ -1,11 +1,19 @@
--- make middle-click copy work in Neovim.
+-- make drag-to-copy work in Neovim.
 
-vim.api.nvim_create_autocmd('CursorMoved', {
-  desc = 'Keep * synced with selection',
-  callback = function()
-    local mode = vim.fn.mode(false)
-    if mode == 'v' or mode == 'V' or mode == '\22' then
-      vim.cmd([[silent norm "*ygv]])
-    end
-  end,
-})
+vim.keymap.set('v', '<LeftRelease>', function()
+  -- 1. Get the raw start (anchor) and end (cursor) positions
+  local _, s_row, s_col, _ = unpack(vim.fn.getpos("v"))
+  local _, e_row, e_col, _ = unpack(vim.fn.getpos("."))
+
+  -- 2. Swap if dragging backwards (upwards or leftwards on the same line)
+  if s_row > e_row or (s_row == e_row and s_col > e_col) then
+    s_row, e_row = e_row, s_row
+    s_col, e_col = e_col, s_col
+  end
+
+  -- 3. Extract the text lines from the buffer safely (adjusting 1-index to 0-index)
+  local lines = vim.api.nvim_buf_get_text(0, s_row - 1, s_col - 1, e_row - 1, e_col, {})
+
+  -- 4. Set the '*' register with our selected lines
+  vim.fn.setreg('*', lines, 'v')
+end, { desc = "Set * register from mouse selection using setreg" })
